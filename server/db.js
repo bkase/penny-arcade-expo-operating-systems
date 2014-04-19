a4a9c5b26375f9118dfc0ba2d83cfc4c250b2961
Brandon Kase <bkase@cmu.edu>
2014-04-19 16:00:25 -0400

    DB might be working

--- server/db.js
+++ server/db.js
@@ -27,17 +27,52 @@ DB.prototype = {
       }
 
       this.transaction(client, this.freeAndDone(freeClient, done), function(revert, finish) {
+        console.log("in this.trnasatction");
         return function() {
-          
-          var query = schema.users.insert({'username':username, 'passhash':this._passwordToHash(username, password)}).toQuery();
-          client.query(query.text, query.values, function(err, result) {
-            if (err) {
-              finish(err);
-              return;
-            }
+          Utils.waterfall([
+            function(next) {
+              console.log("in first waterfall", next);
+              var users = schema.users;
+              var query = users.select(users.star()).from(users).where(users.username.equals(username)).toQuery();
+              console.log("doing query", query);
+              client.query(query.text, query.values, next); 
+            },
 
-            finish(null, true);
-          }.bind(this));
+            function(result, next) {
+              console.log("in second waterfall");
+              if (result.rows.length > 0) {
+                revert(function() {
+                  next(null, true);
+                });
+              } else {
+                next(null, false);
+              }
+            },
+
+            function(isExist, next) {
+              if (isExist) {
+                next(null, true);
+              } else {
+                var query = schema.users.insert({'username':username, 'passhash':this._passwordToHash(username, password)}).toQuery();
+                client.query(query.text, query.values, function(err, res) {
+                  if (err) {
+                    next(err);
+                  } else {
+                    next(null, false);
+                  }
+                });
+              }
+            }.bind(this)
+
+          ], function done(err, isExist) {
+            if (err) {
+              console.log(err);
+              revert(finish);
+              return;
+            } else {
+              finish(null, isExist);
+            }
+          });
         }.bind(this);
       }.bind(this));
     }.bind(this));
@@ -45,9 +80,11 @@ DB.prototype = {
 
   transaction: function(client, done, andThen) {
     client.query('BEGIN', function(err) {
+      console.log("begin transaction");
       var revert = this.rollback(client)
       if (err) return revert(done);
       process.nextTick(andThen(revert, function andThenFinished() {
+        console.log("end transaction");
         var args = Utils.arrayify(arguments);
         args.unshift(null);
         console.log("committing transaction");
@@ -59,6 +96,7 @@ DB.prototype = {
   rollback: function(client) {
     return function(done) {
       client.query('ROLLBACK', function(err) {
+        console.log("Rolling back", err);
         return done(err);
       });
     }.bind(this);
