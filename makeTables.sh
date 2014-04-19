870933d1de476fb1de475b265b07a82790905145
Brandon Kase <bkase@cmu.edu>
2014-04-18 22:27:05 -0400

    Added makeTables script to make the tables

---
+++ makeTables.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CMD="psql -d cloasis -c"
+
+$CMD 'CREATE TABLE apispecs (
+  namespace      varchar(1024),
+  version        real,
+  name           varchar(256),
+  exampleCSVs    text, -- CSV of integers
+  inputSpecType  int, -- id of it in 
+  outputSpecType int -- id
+);'
+
+$CMD 'CREATE TABLE examples (
+  description   text,
+  inputJson     text, -- JSON string
+  outputJson    text  -- JSON string
+);'
+
+$CMD 'CREATE TABLE specTypes (
+  id     int,
+  json   text
+);'
+
