458dbf81b85f0104c560bfa010615795f6469ccb
Brandon Kase <bkase@cmu.edu>
2014-04-19 15:17:11 -0400

    Crazy transaction hack actually working

--- server/db.js
+++ server/db.js
@@ -2,6 +2,7 @@ var crypto = require('crypto');
 var sql = require('sql');
 var schema = require('./schema');
 var pg = require('pg');
+var Utils = require('../common/utils').Utils;
 
 function DB(conString){
   this.conString = conString;
@@ -9,6 +10,13 @@ function DB(conString){
 
 DB.prototype = {
   constructor: DB,
+
+  freeAndDone: function(free, done) {
+    return function() {
+      free();
+      done();
+    };
+  },
 
   createUser: function(username, password, done){
     // TODO: MAKE SURE USER EXISTS
@@ -18,19 +26,43 @@ DB.prototype = {
         return;
       }
 
-      var query = schema.users.insert({'username':username, 'passhash':this._passwordToHash(username, password)}).toQuery();
-      client.query(query.text, query.values, function(err, result) {
-        freeClient();
+      this.transaction(client, this.freeAndDone(freeClient, done), function(revert, finish) {
+        return function() {
+          
+          var query = schema.users.insert({'username':username, 'passhash':this._passwordToHash(username, password)}).toQuery();
+          client.query(query.text, query.values, function(err, result) {
+            if (err) {
+              finish(err);
+              return;
+            }
 
-        if (err) {
-          done(err);
-          return;
-        }
-
-        done(null, true);
+            finish(null, true);
+          }.bind(this));
+        }.bind(this);
       }.bind(this));
     }.bind(this));
   },
+
+  transaction: function(client, done, andThen) {
+    client.query('BEGIN', function(err) {
+      var revert = this.rollback(client)
+      if (err) return revert(done);
+      process.nextTick(andThen(revert, function andThenFinished() {
+        var args = Utils.arrayify(arguments);
+        args.unshift(null);
+        console.log("committing transaction");
+        client.query('COMMIT', done.bind.apply(done, args));
+      }));
+    }.bind(this));
+  },
+
+  rollback: function(client) {
+    return function(done) {
+      client.query('ROLLBACK', function(err) {
+        return done(err);
+      });
+    }.bind(this);
+  }.bind(this),
 
   validateUser: function(username, password, done){
 
