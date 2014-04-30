50fded9320f0c6248067d2b5dbeed355887df09c
Evan Shapiro <evan.a.shapiro@gmail.com>
2014-04-29 21:09:51 -0400

    added hosting for client lib

---
+++ hostClientLib.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var express = require('express');
+var server = express();
+
+fs.readFile('./cloasisLib.js', function(err, file){
+  if (err)
+    throw err;
+  var lib = file.toString();
+ 
+  server.get('/cloasisLib.js', function(request, response){
+    response.send(lib);
+  });
+ 
+  server.listen(20400);
+});
